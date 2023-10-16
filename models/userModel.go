da654e81580b5c35c5117877a4eff3e001d80a9c
Ignacio07 <65515081+Ignacio07@users.noreply.github.com>
2023-10-16 11:38:56 -0300

    Avance

--- models/userModel.go
+++ models/userModel.go
@@ -4,8 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-
-	Id       string `gorm:"not null" json:"id"`
+	
 	Store    string `gorm:"not null" json:"store"`
 	Password string `gorm:"not null" json:"password"`
 }
